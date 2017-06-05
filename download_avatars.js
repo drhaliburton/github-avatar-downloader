fb3e50d9fc7dd990e372fc09545e2201f167aa55
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-05 23:29:20 +0000

    Created gerRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,28 @@
+var GITHUB_USER = "drhaliburton";
+var GITHUB_TOKEN = "443fde915963471c5f9aee62762e491f955c670f";
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+
+// request.get('https://sytantris.github.io/http-examples')               // Note 1
+//        .on('error', function (err) {
+//          throw err;
+//        })
+//        .on('response', function (response) {
+//          console.log('Downloading...');
+//        })
+//        .pipe(fs.createWriteStream('./future.jpg'))
+//        .on('end', function() {
+//          console.log('Download Complete');
+//        });
