ce0c455733a04d18ee57f3804e446c7d7f639c7d
D Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-05 21:35:55 -0700

    Fixed addition error throws for invalid inputted github user data

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,26 @@
 var request = require('request');
 var fs = require('fs');
+var GITHUB_USER = 'drhaliburton';
+var GITHUB_TOKEN = '443fde915963471c5f9aee62762e491f955c670f';
 
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
-// takes in arguments from the command line and performs a get via the GitHub api
 
-function getRepoContributors(owner, name, cb) {
-    var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + owner + '/' + name + '/contributors';
-    var userInfo = {
-        headers: {
-            'User-Agent': 'GitHub Avatar Downloader - Student Project' },
-        url: requestURL
-    };
-    if (owner || name === undefined) {
-        console.log('Invalid entry.');
-        console.log('Valid inputs: node download_avatar.js <repoOwner> <repoName>');
+
+// takes in outputs from getRepoContributors and passes in as arguments to downloadImageByURL
+function getAvatarURL (output) {
+    for (var key in output) {
+      if (output['message'] == 'Not Found') {
+        console.log("Invalid input. Please enter an existing <repoOwner> && <repoName>.");
         return false;
+      }
+        var avatarURL = output[key]['avatar_url'];
+        var newFileName = 'avatars/' + key + '.jpg';
+        downloadImageByURL(avatarURL, newFileName);
+        console.log('Downloading...');
     }
-    request.get(userInfo, function (err, response, body) {
-        if (err) throw err;
-        response.setEncoding('utf8');
-        var parsedBody = JSON.parse(body);
-        cb(parsedBody);
-    });
 }
-
 
 // downloads image based off inputted url and filepath - called in getAvatarURL
 function downloadImageByURL(url, filePath) {
@@ -40,14 +35,28 @@ function downloadImageByURL(url, filePath) {
 }
 
 
-// takes in outputs from getRepoContributors and passes in as arguments to downloadImageByURL
-function getAvatarURL (output) {
-    console.log('Downloading...');
-    for (var key in output) {
-        var avatarURL = output[key]['avatar_url'];
-        var newFileName = 'avatars/' + key + '.jpg';
-        downloadImageByURL(avatarURL, newFileName);
+
+// takes in arguments from the command line and performs a get via the GitHub api
+
+function getRepoContributors(owner, name, cb) {
+    var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + owner + '/' + name + '/contributors';
+    var userInfo = {
+        headers: {
+            'User-Agent': 'GitHub Avatar Downloader - Student Project' },
+        url: requestURL
+    };
+    if (!owner && !name) {
+        console.log('Invalid entry.');
+        console.log('Valid inputs: node download_avatar.js <repoOwner> <repoName>');
+        return false;
     }
+    request.get(userInfo, function (err, response, body) {
+        if (err) throw err;
+        response.setEncoding('utf8');
+        var parsedBody = JSON.parse(body);
+        cb(parsedBody);
+    });
 }
+
 
 getRepoContributors(repoOwner, repoName, getAvatarURL);
