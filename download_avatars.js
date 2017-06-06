6c45a93f8b8e0687d30f70b9d131d04d7cbb0f98
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-06 03:15:35 +0000

    Cleaned indentation

--- download_avatars.js
+++ download_avatars.js
@@ -1,55 +1,55 @@
 var request = require('request');
 var fs = require('fs');
-var GITHUB_USER = "drhaliburton";
-var GITHUB_TOKEN = "443fde915963471c5f9aee62762e491f955c670f";
+var GITHUB_USER = 'drhaliburton';
+var GITHUB_TOKEN = '443fde915963471c5f9aee62762e491f955c670f';
 
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
-function printOutput (output) {
-  console.log('Downloading...');
-  for (var key in output) {
-    var avatarURL = output[key]['avatar_url'];
-    var newFileName = 'avatars/' + key + '.jpg';
-    downloadImageByURL(avatarURL, newFileName);
-  }
-}
+// takes in arguments from the command line and performs a get via the GitHub api
 
 function getRepoContributors(owner, name, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + owner + '/' + name + '/contributors';
-
-  var userInfo = {
-    headers: {
-    "User-Agent": "GitHub Avatar Downloader - Student Project" },
-    url: requestURL
-  }
-
-  if (owner || name === undefined) {
-    console.log("Invalid entry.");
-    console.log("Valid inputs: node download_avatar.js <repoOwner> <repoName>");
-    return false;
-  }
-
-  request.get(userInfo, function (err, response, body) {
-    if (err) throw err;
-    response.setEncoding('utf8');
-
-    var parsedBody = JSON.parse(body);
-
-    cb(parsedBody);
-  });
-
+    var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + owner + '/' + name + '/contributors';
+    var userInfo = {
+        headers: {
+            'User-Agent': 'GitHub Avatar Downloader - Student Project' },
+        url: requestURL
+    };
+    if (owner || name === undefined) {
+        console.log('Invalid entry.');
+        console.log('Valid inputs: node download_avatar.js <repoOwner> <repoName>');
+        return false;
+    }
+    request.get(userInfo, function (err, response, body) {
+        if (err) throw err;
+        response.setEncoding('utf8');
+        var parsedBody = JSON.parse(body);
+        cb(parsedBody);
+    });
 }
 
+
+// downloads image based off inputted url and filepath - called in getAvatarURL
 function downloadImageByURL(url, filePath) {
-  var fileName = './' + filePath;
-  request.get(url)               // Note 1
-         .on('error', function (err) {
-           throw err;
-         })
-         .on('response', function (response) {
-         })
-         .pipe(fs.createWriteStream(fileName));
+    var fileName = './' + filePath;
+    request.get(url)               // Note 1
+           .on('error', function (err) {
+               throw err;
+           })
+           .on('response', function (response) {
+           })
+           .pipe(fs.createWriteStream(fileName));
 }
 
-getRepoContributors(repoOwner, repoName, printOutput);
+
+// takes in outputs from getRepoContributors and passes in as arguments to downloadImageByURL
+function getAvatarURL (output) {
+    console.log('Downloading...');
+    for (var key in output) {
+        var avatarURL = output[key]['avatar_url'];
+        var newFileName = 'avatars/' + key + '.jpg';
+        downloadImageByURL(avatarURL, newFileName);
+    }
+}
+
+getRepoContributors(repoOwner, repoName, getAvatarURL);
