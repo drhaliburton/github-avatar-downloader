b86f02aa97994b7913f3b1a4cd7631558c77fc39
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-06 01:48:10 +0000

    added download avatar functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,40 +1,53 @@
+var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "drhaliburton";
 var GITHUB_TOKEN = "443fde915963471c5f9aee62762e491f955c670f";
-var request = require('request');
-
 
 function printOutput (output) {
-  console.log(output);
-}
-
-
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-
-  var userInfo = {
-    headers: {
-    "User-Agent": "GitHub Avatar Downloader - Student Project" },
-    url: requestURL
+  for (var key in output) {
+    console.log(output['avatar_url']);
   }
+}
 
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+//
+//   var userInfo = {
+//     headers: {
+//     "User-Agent": "GitHub Avatar Downloader - Student Project" },
+//     url: requestURL
+//   }
+//
+//   request.get(userInfo, function (err, response, body) {
+//     response.setEncoding('utf8');
+//
+//     if (err) throw err;
+//
+//     response.on('end', function(parsedBody) {
+//       var parsedBody = JSON.parse(body);
+//       callback(parsedBody);
+//     });
+//   });
+// }
 
-  request.get(userInfo, function (err, response, body) {
-    if (err) throw err;
-    response.setEncoding('utf8');
+function downloadImageByURL(url, filePath) {
 
-    var bodyResult = "";
-    response.on('data', function (data) {
-        for (var i = 0; i < data.length; i++) {
-          bodyResult += data[i].body;
-        }
-      });
-    response.on('end', function() {
-      callback(dataResult);
-    });
-  });
+  request.get(url)               // Note 1
+         .on('error', function (err) {
+           throw err;
+         })
+         .on('response', function (response) {
+           console.log('Downloading...');
+         })
+         .pipe(fs.createWriteStream('./newAvatar.jpg'))
+         .on('end', function() {
+           console.log('Download Complete');
+         });
+
 }
 
 
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
-getRepoContributors('jquery', 'jquery', printOutput);
+
+// getRepoContributors('jquery', 'jquery', printOutput);
